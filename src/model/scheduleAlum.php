0db1957cb534a21cec3d7f36db7de3b0515b4276
GreenNugget <naomi.snchz99@gmail.com>
2020-04-25 17:46:00 -0500

    Se cambió el nombre de la función que muestra los datos

--- src/model/scheduleAlum.php
+++ src/model/scheduleAlum.php
@@ -5,15 +5,15 @@ $idGroup = "G123";//Aquí se cambiaría por el valor del grupo obtenido de acuer
 
 if($conexion = connectToDB()){
 
-    //Consulta para obtener algunos datos del horario
+    //Consulta para obtener los datos del horario
     $sql = "select nombre_profesor,nombre_mate,hora_inicio,hora_final,dias_impartidos,descripcion from 
     (((((horarios join clases)
     join sesiones)join aulas on aulas.id_aula=sesiones.id_aula)
     join profesores on clases.clv_profe=profesores.clv_profe)
     join materias on clases.clv_materia=materias.clv_materia)
     where id_grupo='$idGroup' AND horarios.id_clase=clases.id_clase AND horarios.id_horario=sesiones.id_horario";
-    $result = $conexion->query($sql);
-    getHorariosData($result);
+    $result = consultToDB($sql);
+    getHorarioAlumno($result);
 
 
     $conexion->close();//Se cierra la conexión por fin
