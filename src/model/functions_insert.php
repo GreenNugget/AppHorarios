579ce2bbe4834bf3f8e06768d2d4f5e3f5e6cb59
GreenNugget <naomi.snchz99@gmail.com>
2020-04-27 17:46:40 -0500

    The functions are ready.

--- src/model/functions_insert.php
+++ src/model/functions_insert.php
@@ -1,6 +1,5 @@
 <?php
 
-/* */
 function connectToDB(){
     return mysqli_connect('localhost', 'root', '', 'schedexproject');
 }
@@ -30,6 +29,7 @@ function getLastID($table){
             return 'S001';
         }
     }
+
 }
 
 /* Function to create a new key to insert data into the right table */
@@ -58,19 +58,23 @@ function createNewId($table){
     return $newId;
 }
 
+/* Function to get the key of the profesor, the subject or the classroom based on the name */
+function getKey($table,$name){
+    $conexion = connectToDB();
+    $sql = '';
 
+    if ($table == 'profesores') {
+        $sql = "select clv_profe from profesores where nombre_profesor='$name'";
+    } elseif ($table == 'materias') {
+        $sql = "select clv_materia from materias where nombre_mate='$name'";
+    }elseif($table == 'aulas'){
+        $sql = "select id_aula from aulas where descripcion='$name'";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    $result = mysqli_query($conexion, $sql);
+    if ($row = mysqli_fetch_row($result)) {
+        return trim($row[0]);
+    }
+}
 
 ?>
