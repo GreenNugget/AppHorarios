7263a958c54829e855fcedca5e24d148925c1c3b
GreenNugget <naomi.snchz99@gmail.com>
2020-04-27 17:49:31 -0500

    The sentences for the insertions work. (We still need to change the values on the top tho)

--- src/model/createSchedule.php
+++ src/model/createSchedule.php
@@ -1,29 +1,49 @@
 <?php
 include 'functions_insert.php';
 
-$idClass= createNewId('clases');
-$idHorar = createNewId('horarios');
-$idSession = createNewId('sesiones');
+//Logic to recover all the info (JSON format)
 
-/*This is just a trialx2
-echo "<br>";
-echo "<br> FINALES:";
-echo "<br>";
-echo $idClass;
-echo "<br>";
-echo $idHorar;
-echo "<br>";
-echo $idSession;*/
-/*
+//Then we are supposed to have all the data that is required to fill the table in the db, like this:
+$day = 'Lunes';
+$group = 'G789';
+$classroom = 'C5';
+$start_hour = '01:30';
+$finish_hour = '03:00';
+$profesor_name = 'Luis Mario Góngora León';
+$subject_name = 'Bases de datos';
+
+//We need some keys to add rows to the tables, so we search them
+$idClass= createNewId('clases');
+$idSched = createNewId('horarios');
+$idSession = createNewId('sesiones');
+$clv_Profe = getKey('profesores',$profesor_name);
+$clv_Subj = getKey('materias',$subject_name);
+$clv_classroom = getKey('aulas',$classroom);
+
 $conexion = connectToDB();
 if($conexion){
-    $sql = "INSERT INTO `usuarios` (`role`, `userName`, `userPassword`) VALUES ('1', 'prueba', 'pruebita1')";
-    $resultado = $conexion->query($sql);
+    $sql_class = "INSERT INTO `clases` (`id_clase`, `clv_profe`, `clv_materia`, `hora_inicio`, `hora_final`) VALUES ('$idClass', '$clv_Profe', '$clv_Subj', '$start_hour', '$finish_hour')";
+    $resultado = $conexion->query($sql_class);
 
     if ($resultado) {
-        echo "SE GUARDÓ TODO BRO";
+        echo "SE GUARDÓ LA CLASE";
     }
+
+    $sql_schedule = "INSERT INTO `horarios` (`id_horario`, `id_grupo`, `id_clase`) VALUES ('$idSched', '$group', '$idClass')";
+    $resultado = $conexion->query($sql_schedule);
+
+    if ($resultado) {
+        echo "SE GUARDÓ EL HORARIO";
+    }
+
+    $sql_session = "INSERT INTO `sesiones` (`clv_sesion`, `id_aula`, `id_horario`, `dias_impartidos`) VALUES ('$idSession', '$clv_classroom', '$idSched', '$day')";
+    $resultado = $conexion->query($sql_session);
+
+    if ($resultado) {
+        echo "SE GUARDÓ LA SESIÓN";
+    }
+
     $conexion->close();
-}*/
+}
 
 ?>
