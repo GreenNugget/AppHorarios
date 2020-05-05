95ddd215b298d0246ab6d156ab672bf2cff9bb6f
GreenNugget <naomi.snchz99@gmail.com>
2020-05-05 02:32:13 -0500

    Now we receive a JSON format from the front

--- src/model/createSchedule.php
+++ src/model/createSchedule.php
@@ -2,37 +2,42 @@
 include 'functions_insert.php';
 
 //we receive a JSON format
-$horariosJSON = file_get_contents("a.json");
+$horariosJSON = file_get_contents("info.json");
 
+$isSaved = false;
 $conexion = connectToDB();
 if ($conexion){
-    $horariosDecoded = json_decode($horariosJSON);
+    $horariosDecoded = json_decode($horariosJSON);//we extract the info
     
-    foreach ($horariosDecoded->clases as $mydata) {
+    foreach ($horariosDecoded->clases as $clases) {
         $group = $horariosDecoded->nombreGrupo;
         $idClass = createNewId('clases');
         $idSched = createNewId('horarios');
-        $clv_Profe = getKey('profesores', $mydata->profesor->nombre_profesor); //nombre del prof
-        $clv_Subj = getKey('materias', $mydata->materia->nombre_mate); //nombre de la materia
-        $clv_classroom = getKey('aulas', $mydata->aula->descripcion); //nombre del aula
-        $sesiones = json_encode($mydata->sesiones); //JSON de las sesiones
+        $clv_Profe = getKey('profesores', $clases->profesor->nombre_profesor); //nombre del prof
+        $clv_Subj = getKey('materias', $clases->materia->nombre_mate); //nombre de la materia
+        $clv_classroom = getKey('aulas', $clases->aula->descripcion); //nombre del aula
+        $sesiones = json_encode($clases->sesiones); //JSON de las sesiones
         
         $sql_class = "INSERT INTO `clases` (`id_clase`, `clv_profe`, `clv_materia`) VALUES ('$idClass', '$clv_Profe', '$clv_Subj')";
         $resultado = $conexion->query($sql_class);
 
         if ($resultado) {
-            echo "SE GUARDÓ LA CLASE<br>";
+            $isSaved = true;
         }
         
         $sql_schedule = "INSERT INTO `horarios` (`id_horario`, `id_grupo`, `id_clase`,`id_aula`, `sesiones`) VALUES ('$idSched', '$group', '$idClass', '$clv_classroom', '$sesiones')";
         $resultado = $conexion->query($sql_schedule);
         
-        if ($resultado) {
-            echo "<br>SE GUARDÓ EL HORARIO";
+        if (!$resultado) {
+            $isSaved = false;
         }
 
     }
     $conexion->close();
+}
+
+if($isSaved = true){
+    //We can put a message to tell the admin that the data was saved;
 }
 
 ?>
