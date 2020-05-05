018b559b6d78e66e48b2d33b1344acf0d0f75837
GreenNugget <naomi.snchz99@gmail.com>
2020-05-05 03:54:55 -0500

    Now we can update the db without problems :')

--- src/model/functions_update.php
+++ src/model/functions_update.php
@@ -3,10 +3,6 @@ function connectToDB(){
     return mysqli_connect('localhost', 'root', '', 'schedexproject');
 }
 
-function getTableID(){
-    
-}
-
 /* Function to get the key of the profesor, the subject or the classroom based on the name */
 function getKey($table, $name){
     $conexion = connectToDB();
@@ -19,6 +15,19 @@ function getKey($table, $name){
     } elseif ($table == 'aulas') {
         $sql = "select id_aula from aulas where descripcion='$name'";
     }
+
+    $result = mysqli_query($conexion, $sql);
+    if ($row = mysqli_fetch_row($result)) {
+        return trim($row[0]);
+    }
+}
+
+/* Function to get the class id */
+function getClassID($profKey,$subjKey){
+
+    $conexion =connectToDB();
+
+    $sql = "select id_clase from clases where clv_profe='$profKey' and clv_materia='$subjKey'";
 
     $result = mysqli_query($conexion, $sql);
     if ($row = mysqli_fetch_row($result)) {
