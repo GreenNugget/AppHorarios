25d4e0a596da76f89e77a07a4160c7d1fae50a8c
GreenNugget <naomi.snchz99@gmail.com>
2020-05-09 21:47:40 -0500

    The problems with the insertions are over.

--- src/model/functions_insert.php
+++ src/model/functions_insert.php
@@ -4,33 +4,48 @@ function connectToDB(){
     return mysqli_connect('localhost', 'root', '', 'schedexproject');
 }
 
-/* Function to get the last Id from the schedules tables */
 function getLastID($table){
     $conexion = connectToDB();
-    $sql='';
-    
-    if($table=='clases'){
+    $sql = '';
+
+    if ($table == 'clases') {
         $sql = "SELECT MAX(id_clase) AS id_clase FROM clases";
-    }elseif($table == 'horarios'){
+    } elseif ($table == 'horarios') {
         $sql = "SELECT MAX(id_horario) AS id_horario FROM horarios";
     }
 
-    $resultado = mysqli_query($conexion,$sql);
+    $resultado = mysqli_query($conexion, $sql);
     if ($row = mysqli_fetch_row($resultado)) {
         return trim($row[0]);
-    }else{
+    } else {
         return '0';
     }
-
 }
 
 /* Function to create a new key to insert data into the right table */
 function createNewId($table){
     $actualId = getLastID($table);
 
-    $actualId++;
+    $aux = substr($actualId, 1); //The letter of the key is subtracted to increase the index
+    if($aux==0){
+        $aux = 1;
+    }else{
+        $aux++;
+    }
 
-    return $actualId;
+    if (strlen($aux) == 1) {
+        $aux = '00' . $aux;
+    } elseif (strlen($aux) == 2) {
+        $aux = '0' . $aux;
+    }
+
+    $newId = ""; //The start of the key is concatenated according to the table
+    if ($table == 'clases') {
+        $newId = 'C' . $aux;
+    } elseif ($table == 'horarios') {
+        $newId = 'H' . $aux;
+    }
+    return $newId;
 }
 
 /* Function to get the key of the profesor, the subject or the classroom based on the name */
