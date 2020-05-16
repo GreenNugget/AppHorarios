eddd95a168c9851011d689f278b2f7f74ea552a1
Chesare22 <Chesare22@github.com>
2020-05-15 21:02:13 -0500

    feat: login.php uses response codes

--- src/sesiones/login.php
+++ src/sesiones/login.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
 /*
 Archivo que sirve para la validacion de un login
 */
@@ -10,7 +12,6 @@ require 'database.php';
 //recibe el usuario y la contraseña.
 $username = $_POST['username'];
 $password = $_POST['password'];
-
 //Lectura de la base de datos de la tabla que se necesita.
 if (!empty($username) && !empty($password)){
     $record = $conn->prepare('SELECT role, userName, userPassword FROM usuarios WHERE userName = :username');
@@ -22,15 +23,18 @@ if (!empty($username) && !empty($password)){
 
     if(count($result) > 0 && ($password == $result['userPassword'])){
         $_SESSION['user_id'] = $result['userName'];//la salida es el id de usuario.
-        header("Location: /sesiones/index.php");//cambiar a donde se redireccionará una vez ingresado
+        echo '"Se ingresó correctamente"';
+        // header("Location: /sesiones/index.php");//cambiar a donde se redireccionará una vez ingresado
     }else{
         $message = 'Sus datos no coinsiden';
-        echo '<script language="javascript">alert("Sus datos no coinsiden");</script>';
-        header("Location: /sesiones/login.html");//redireccionar al front del login 
+        http_response_code(401);
+        echo $message;
+        // header("Location: /sesiones/login.html");//redireccionar al front del login 
     }
 }else{
     $message = 'Ingrese sus datos';
-    echo '<script language="javascript">alert("Ingrese sus datos");</script>';
-    header("Location: /sesiones/login.html");//redireccionar al front del login
+        http_response_code(401);
+        echo $message;
+    // header("Location: /sesiones/login.html");//redireccionar al front del login
 }
 ?>
