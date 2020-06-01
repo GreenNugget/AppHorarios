5b3decfec2f0c0ce4f7674262ea9fa5cc5466f01
Chesare22 <Chesare22@github.com>
2020-06-01 12:42:14 -0500

    bug: $conn no se declaraba

--- src/sesiones/database.php
+++ src/sesiones/database.php
@@ -3,8 +3,8 @@
 Este archivo sirve para conectar a la base de datos
 se llama con 'require 'database.php';' al inicio de archivo
 */
-$dbInfo = json_decode(file_get_contents("../src/model/db_info.json"));
-return mysqli_connect($dbInfo->host, $dbInfo->user, $dbInfo->password, $dbInfo->database);
+$dbInfo = json_decode(file_get_contents("../model/db_info.json"));
+mysqli_connect($dbInfo->host, $dbInfo->user, $dbInfo->password, $dbInfo->database);
 
 try {
     $conn = new PDO("mysql:host=$dbInfo->host;dbname=$dbInfo->database;", $dbInfo->user, $dbInfo->password);
